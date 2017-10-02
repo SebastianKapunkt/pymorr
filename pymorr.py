0451530339245fbcf64c0c0d54b2a48185033877
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2017-10-02 18:32:54 +0200

    Adds get_images_of_root function

--- pymorr.py
+++ pymorr.py
@@ -1,13 +1,17 @@
 import os
+import glob
 
 class Pymorr:
     def __init__(self):
-        self.root = ""
+        self.root = "" # represents the root path for all operations
+        self.supported_types = ('*.jpg', '*.png', '*.jpeg')
 
     def set_root(self, input):
-        """Sets the root path for all operations.
-         Parameters
-         ----------
+        """
+        Sets the root path for all operations.
+
+        Parameters
+        ----------
             input : str
                 a valid path to set as root
         """
@@ -21,3 +25,17 @@ class Pymorr:
             return os.listdir(self.root)
         else:
             return os.listdir()
+
+    def get_images_of_root(self):
+        """
+        finds all images of root path
+
+        Returns
+        -------
+        list of str
+            path of image files with root path
+        """
+        files_grabbed = []
+        for files in self.supported_types:
+            files_grabbed.extend(glob.glob(os.path.join(self.root, files)))
+        return files_grabbed
