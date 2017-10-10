d21d94d4bfc2c8fe8badaba2e8b0cce8f400a0d4
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2017-10-10 11:41:24 +0200

    Adds log on move of image

--- pymorr.py
+++ pymorr.py
@@ -6,6 +6,7 @@ class Pymorr:
         self.root = "" # represents the root path for all operations
         self.supported_types = ('*.jpg', '*.png', '*.jpeg')
         self.prefered_folder = {'Keep': 'keep', 'Delete': 'delete', 'Maybe': 'maybe'}
+        self.image_index = 'index.txt'
 
     def set_root(self, input):
         """
@@ -45,6 +46,7 @@ class Pymorr:
         """
         Moves an image to a desired folder under root and checks before
         if the folder exists. 'root/folder_to_move'
+        Also adds the move to the log file 'index.txt'. 
 
         Parameters
         ----------
@@ -64,6 +66,7 @@ class Pymorr:
         directory = self.create_if_not_exists(folder_to_move)
         destination = os.path.join(directory, os.path.basename(image))
         os.rename(image, os.path.join(image, destination))
+        self.add_image_to_log(image,destination)
         return destination
 
     def create_if_not_exists(self, folder):
@@ -84,3 +87,24 @@ class Pymorr:
         if not os.path.exists(directory):
             os.makedirs(directory)
         return directory
+
+    def add_image_to_log(self, path_before, path_after):
+        """
+        Appends two paths to the bottom of a file.
+
+        Parameters
+        ----------
+        path_before : str
+            The full path before a change.
+
+        path_after : str
+            The full path after a change.
+        """
+        image_index_path = os.path.join(self.root,self.image_index)
+
+        if os.path.isfile(image_index_path):
+            with open(image_index_path, "a") as myfile:
+                myfile.write("\n'{}','{}'".format(path_before, path_after))
+        else:
+            with open(image_index_path, "a") as myfile:
+                myfile.write("'{}','{}'".format(path_before, path_after))
